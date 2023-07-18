ba0250bd206e6dfad9ea03a33a91632ef75bfde1
MD Sahebuzzaman Fahad <112535621+fahad-web@users.noreply.github.com>
2023-07-18 14:32:09 +0600

    mid

---
+++ MD. SAHEBUZZAMAN FAHAD/Mid Project/src/traveler/traveler.entity.ts
@@ -0,0 +1,26 @@
+import { AdminEntity } from "src/admin/admin.entity";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('traveler')
+export class TravelerEntity {
+    @PrimaryGeneratedColumn()
+    id:number;
+    @Column({length:150})
+    fastname:string;
+    @Column({length:80})
+    lastname:string;
+    @Column({unique:true})
+    email:string;
+    @Column()
+    contact:number;
+    @Column()
+    password:string;
+    @Column({nullable:true})
+    photoFileName:string;
+    @Column({nullable:true})
+    adminID :number
+
+    @ManyToOne(() => AdminEntity, admin => admin.manager, {onDelete:"CASCADE"})
+    @JoinColumn({name:'adminID'})
+    admin: AdminEntity;
+}
